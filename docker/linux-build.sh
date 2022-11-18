9eda8444f10634007e0bc4c2f201b976a4a63b32
Dan <dan@wadoku.de>
2022-11-18 21:36:36 +0100

    update linux-build.sh

--- docker/linux-build.sh
+++ docker/linux-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec docker run --rm -it --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data makigumo/hoppersdk-linux-docker ./build.sh
+exec docker run --rm --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data makigumo/hoppersdk-linux-docker ./build.sh
